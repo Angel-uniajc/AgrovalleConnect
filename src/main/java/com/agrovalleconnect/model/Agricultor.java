d76c65a51c603c5fcf28c89342807083d56ef695
Angel Isaac Castillo Cuesta <aisaaccastillo@estudiante.uniajc.edu.co>
2026-09-24 23:12:44 -0500

    chore: se realizó el contructor vacio y sus respectivos setters and getters

--- src/main/java/com/agrovalleconnect/model/Agricultor.java
+++ src/main/java/com/agrovalleconnect/model/Agricultor.java
@@ -24,7 +24,59 @@ public class Agricultor {
 
     @Column (name = "fecha_registro")
     private java.time.LocalDate fechaRegistro;
-    
 
+    // Constructor vacio
+
+    public Agricultor() {}
+
+    //getters y setters
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDocumentoIdentidad() {
+        return documentoIdentidad;
+    }
+
+    public void setDocumentoIdentidad(String documentoIdentidad) {
+        this.documentoIdentidad = documentoIdentidad;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public java.time.LocalDate getFechaRegistro() {
+        return fechaRegistro;
+    }
+
+    public void setFechaRegistro(java.time.LocalDate fechaRegistro) {
+        this.fechaRegistro = fechaRegistro;
+    }
 
 }
