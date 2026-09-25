fbb60f24c6bf56310760bc914c79145dd85e24c4
Angel Isaac Castillo Cuesta <aisaaccastillo@estudiante.uniajc.edu.co>
2026-09-24 23:02:09 -0500

    chore: Se realizaron las columnas para la entidad Agricultor

--- src/main/java/com/agrovalleconnect/model/Agricultor.java
+++ src/main/java/com/agrovalleconnect/model/Agricultor.java
@@ -6,5 +6,25 @@ import jakarta.persistence.*;
 @Table(name = "agricultor")
 public class Agricultor {
     
+    @Id 
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column (nullable = false, length = 100)
+    private String nombre;
+
+    @Column (nullable = false, length = 20)
+    private String documentoIdentidad;
+
+    @Column (nullable = false, length = 20)
+    private String telefono;
+
+    @Column (length = 100)
+    private String correo;
+
+    @Column (name = "fecha_registro")
+    private java.time.LocalDate fechaRegistro;
     
+
+
 }
